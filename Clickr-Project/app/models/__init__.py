0dd5bbec74c49a21406ae09ea58059b29d53d93b
richa garg <richa@Richas-iMac.local>
2024-02-07 19:57:03 -0500

    Database Created

--- Clickr-Project/app/models/__init__.py
+++ Clickr-Project/app/models/__init__.py
@@ -1,3 +1,8 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .album import Album
+from .photo import Photo
+from .comment import Comment
+from .favorite import Favorite
+
---
+++ Clickr-Project/app/models/album.py
@@ -0,0 +1,27 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from .photoalbum import photoalbums
+
+
+class Album(db.Model, UserMixin):
+    __tablename__ = 'albums'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    description = db.Column(db.String(50))
+    userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
+
+    user = db.relationship("User", back_populates="albums")
+    photos = db.relationship("Photo", secondary=photoalbums, back_populates="albums")
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'userId': self.userId,
+        }
