40658a34c20d9f806646bbfb2e67012a635672aa
Komal aher <komalaher18@gmail.com>
2024-02-14 17:25:06 -0800

    login/signup Modal

--- react-vite/src/components/SignupFormModal/SignupFormModal.jsx
+++ react-vite/src/components/SignupFormModal/SignupFormModal.jsx
@@ -7,27 +7,31 @@ import "./SignupForm.css";
 function SignupFormModal() {
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
+  // const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  // const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (password !== confirmPassword) {
-      return setErrors({
-        confirmPassword:
-          "Confirm Password field must be the same as the Password field",
-      });
-    }
+    // if (password !== confirmPassword) {
+    //   return setErrors({
+    //     confirmPassword:
+    //       "Confirm Password field must be the same as the Password field",
+    //   });
+    // }
 
     const serverResponse = await dispatch(
       thunkSignup({
         email,
-        username,
+        // username,
         password,
+        firstName,
+        lastName,
       })
     );
 
@@ -40,20 +44,39 @@ function SignupFormModal() {
 
   return (
     <>
-      <h1>Sign Up</h1>
+      <i className="fa-brands fa-flickr"></i>
+      <h1>Sign Up for Flickr</h1>
       {errors.server && <p>{errors.server}</p>}
       <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
+        <input
+          placeholder="First name"
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+
+        {errors.firstName && <p>{errors.firstName}</p>}
+        <input
+          placeholder="Last name"
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+
+        {errors.lastName && <p>{errors.lastName}</p>}
+
+        <input
+          placeholder="Email address"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
         {errors.email && <p>{errors.email}</p>}
-        <label>
+        {/* <label>
           Username
           <input
             type="text"
@@ -62,18 +85,18 @@ function SignupFormModal() {
             required
           />
         </label>
-        {errors.username && <p>{errors.username}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
+        {errors.username && <p>{errors.username}</p>} */}
+
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
         {errors.password && <p>{errors.password}</p>}
-        <label>
+        {/* <label>
           Confirm Password
           <input
             type="password"
@@ -82,7 +105,7 @@ function SignupFormModal() {
             required
           />
         </label>
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+        {errors.confirmPassword && <p>{errors.confirmPassword}</p>} */}
         <button type="submit">Sign Up</button>
       </form>
     </>
