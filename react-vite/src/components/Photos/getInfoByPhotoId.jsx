6b380fe099f085051551520e90be6d9e2203ae19
irelius <kihong711@hotmail.com>
2024-02-22 18:40:12 -0800

    creating new branch to test deployment using a different branch

--- react-vite/src/components/Photos/getInfoByPhotoId.jsx
+++ react-vite/src/components/Photos/getInfoByPhotoId.jsx
@@ -23,116 +23,125 @@ function GetPhotoDetails() {
 
   return (
     <div>
-      {photoDetails ? (
-        <div>
-          <div>
-            <h1>Photo Details</h1>
-            <div>
-              <p>Title: {photoDetails.title}</p>
-              <p>Description: {photoDetails.description}</p>
-              <img src={photoDetails.url} alt={photoDetails.title} />
-            </div>
-          </div>
-
-// <<<<<<< AD/frontend
-//           <div>
-//             <h1>Comments</h1>
-//             {allComments.length === 0 ? (
-//               <div>
-//                 <p>Be the first to comment!</p>
-//               </div>
-//             ) : (
-//               allComments.map((comment) => (
-//                 <div key={comment.id}>
-//                   <div>
-//                     {comment.userName} : {comment.comment}
-//                   </div>
-//                   {currentUser && currentUser.id === comment.userId && (
-//                     <span>
-//                       <OpenModalButton
-//                         buttonText={"Edit Comment"}
-//                         modalComponent={
-//                           <EditComment props={{ comment: comment, photoId: id }} />
-//                         }
-//                       />
-//                       <OpenModalButton
-//                         buttonText={"Delete Comment"}
-//                         modalComponent={<DeleteComment comment={comment} />}
-//                       />
-//                     </span>
-//                   )}
-//                 </div>
-//               ))
-//             )}
-//           </div>
-//           <div>
-//             {currentUser &&
-//               photoDetails &&
-//               currentUser.id !== photoDetails.userId && (
-//                 <div>
-//                   <CreateNewComment photo={photoDetails} />
-//                 </div>
-//               )}
-//           </div>
-//         </div>
-      ) : (
-        <div>
-          <h1>404 - Not Found</h1>
-          <p>The photo you're looking for does not exist.</p>
-        </div>
-      )}
-//=======
-      <div>
-        <h3>comments</h3>
-        <div>
-          {allComments.length === 0 && (
-            <span className="">
-              <div>
-                <h3>Be the first person to comment</h3>
-              </div>
-            </span>
-          )}
-        </div>
-        <div>
-          {currentUser && photoDetails && currentUser.id !== photoDetails.userId &&
-        (
-        <div>
-          <CreateNewComment photo={photoDetails} />
-        </div>
-        )}
-      </div>
-      <div>
-        {allComments.map((comment) => (
-          <div key={comment.id}>
-            <div>
-              {comment.userName} : {comment.comment}
-            </div>
-
-            {currentUser && currentUser.id == comment.userId && (
-              <span>
-                <OpenModalButton
-                  buttonText={"Edit Comment"}
-                  modalComponent={
-                    <EditComment props={{ comment: comment, photoId: id }} />
-                  }
-                />
-
-                <OpenModalButton
-                  buttonText={"Delete Comment"}
-                  modalComponent={<DeleteComment comment={comment} />}
-                />
-              </span>
-            )}
-          </div>
-        ))}
-
-
-      </div>
-      </div>
-//>>>>>>> staging
+        asdf
     </div>
   )
+
 }
+
+//   return (
+//     <div>
+//       {photoDetails ? (
+//         <div>
+//           <div>
+//             <h1>Photo Details</h1>
+//             <div>
+//               <p>Title: {photoDetails.title}</p>
+//               <p>Description: {photoDetails.description}</p>
+//               <img src={photoDetails.url} alt={photoDetails.title} />
+//             </div>
+//           </div>
+
+// {/* // <<<<<<< AD/frontend
+// //           <div>
+// //             <h1>Comments</h1>
+// //             {allComments.length === 0 ? (
+// //               <div>
+// //                 <p>Be the first to comment!</p>
+// //               </div>
+// //             ) : (
+// //               allComments.map((comment) => (
+// //                 <div key={comment.id}>
+// //                   <div>
+// //                     {comment.userName} : {comment.comment}
+// //                   </div>
+// //                   {currentUser && currentUser.id === comment.userId && (
+// //                     <span>
+// //                       <OpenModalButton
+// //                         buttonText={"Edit Comment"}
+// //                         modalComponent={
+// //                           <EditComment props={{ comment: comment, photoId: id }} />
+// //                         }
+// //                       />
+// //                       <OpenModalButton
+// //                         buttonText={"Delete Comment"}
+// //                         modalComponent={<DeleteComment comment={comment} />}
+// //                       />
+// //                     </span>
+// //                   )}
+// //                 </div>
+// //               ))
+// //             )}
+// //           </div>
+// //           <div>
+// //             {currentUser &&
+// //               photoDetails &&
+// //               currentUser.id !== photoDetails.userId && (
+// //                 <div>
+// //                   <CreateNewComment photo={photoDetails} />
+// //                 </div>
+// //               )}
+// //           </div>
+// //         </div>
+//       ) : (
+//         <div>
+//           <h1>404 - Not Found</h1>
+//           <p>The photo you're looking for does not exist.</p>
+//         </div>
+//       )}
+// //======= */}
+//       <div>
+//         <h3>comments</h3>
+//         <div>
+//           {allComments.length === 0 && (
+//             <span className="">
+//               <div>
+//                 <h3>Be the first person to comment</h3>
+//               </div>
+//             </span>
+//           )}
+//         </div>
+//         <div>
+//           {currentUser && photoDetails && currentUser.id !== photoDetails.userId &&
+//         (
+//         <div>
+//           <CreateNewComment photo={photoDetails} />
+//         </div>
+//         )}
+//       </div>
+//       <div>
+//         {allComments.map((comment) => (
+//           <div key={comment.id}>
+//             <div>
+//               {comment.userName} : {comment.comment}
+//             </div>
+
+//             {currentUser && currentUser.id == comment.userId && (
+//               <span>
+//                 <OpenModalButton
+//                   buttonText={"Edit Comment"}
+//                   modalComponent={
+//                     <EditComment props={{ comment: comment, photoId: id }} />
+//                   }
+//                 />
+
+//                 <OpenModalButton
+//                   buttonText={"Delete Comment"}
+//                   modalComponent={<DeleteComment comment={comment} />}
+//                 />
+//               </span>
+//             )}
+//           </div>
+//         ))}
+
+
+//       </div>
+//       </div>
+// {/* //>>>>>>> staging */}
+//     </div>
+//   )
+// }
+
 
 
 
