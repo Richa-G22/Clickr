386e15001e3e7889cb8e0819c3b9a7720315560d
LanaSimone <allanadecarishs@gmail.com>
2024-02-25 11:21:00 -0500

    trying to fix favorites

--- app/api/favorite_routes.py
+++ app/api/favorite_routes.py
@@ -6,18 +6,21 @@ from flask_login import login_required
 
 favorite_routes = Blueprint('favorites', __name__)
 
+
 # Get all favorite photos for the logged in User
 @favorite_routes.route('/all')
 @login_required
 def all_favorites():
     all_favorites = Favorite.query.filter_by(userId=current_user.id).all()
-    favorite_list = [{
-                   'id': favorite.id,
-                   'photoId': favorite.photoId,
-                   'userId': favorite.userId
-                } for favorite in all_favorites]
+    favorite_list = [
+        {"id": favorite.id, "photoId": favorite.photoId, "userId": favorite.userId}
+        for favorite in all_favorites
+    ]
 
-    return jsonify(favorite_list)
+    # Ensure the response always contains the 'favorites' field
+    response = {"userId": current_user.id, "favorites": favorite_list}
+
+    return jsonify(response)
 
 
 # Favorite a photo
